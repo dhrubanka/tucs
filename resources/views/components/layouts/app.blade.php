be3d356cf89106f216b82eb370c15af0d968ec98
ravenDC010 <csm19033@tezu.ac.in>
2021-09-22 01:03:48 +0530

    profile

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -73,7 +73,7 @@
                             @endif
                         @else
                             <li class="nav-item ">
-                                <a id="navbarDropdown" class="nav-link " href="#"  >
+                                <a id="navbarDropdown" class="nav-link " href="/profile"  >
                                     {{ Auth::user()->name }}
                                 </a>
                             </li>
@@ -99,5 +99,7 @@
             {{ $slot }}
         </main>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </body>
 </html>
