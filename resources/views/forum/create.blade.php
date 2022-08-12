416bb60db37fa79a9e5d7e777c23ab2148feef58
Dhrubanka Chutia <dhrubankachutia@gmail.com>
2022-08-13 02:37:48 +0530

    update ckeditor

--- resources/views/forum/create.blade.php
+++ resources/views/forum/create.blade.php
@@ -44,9 +44,50 @@
 
 <script src="/ckeditor/ckeditor.js"></script>
 <script type="text/javascript">
-        CKEDITOR.replace('content', {
+    CKEDITOR.replace('content', {
             filebrowserUploadUrl: "{{route('ckeditor.image-upload', ['_token' => csrf_token() ])}}",
             filebrowserUploadMethod: 'form'
         });
+    CKEDITOR.on('instanceReady', function (ev) {
+        ev.editor.dataProcessor.htmlFilter.addRules( {
+        elements : {
+            img: function( el ) {
+                // Add bootstrap "img-responsive" class to each inserted image
+                el.addClass('img-responsive');
+        
+                // Remove inline "height" and "width" styles and
+                // replace them with their attribute counterparts.
+                // This ensures that the 'img-responsive' class works
+                var style = el.attributes.style;
+        
+                if (style) {
+                    // Get the width from the style.
+                    var match = /(?:^|\s)width\s*:\s*(\d+)px/i.exec(style),
+                        width = match && match[1];
+        
+                    // Get the height from the style.
+                    match = /(?:^|\s)height\s*:\s*(\d+)px/i.exec(style);
+                    var height = match && match[1];
+        
+                    // Replace the width
+                    if (width) {
+                        el.attributes.style = el.attributes.style.replace(/(?:^|\s)width\s*:\s*(\d+)px;?/i, '');
+                        el.attributes.width = width;
+                    }
+        
+                    // Replace the height
+                    if (height) {
+                        el.attributes.style = el.attributes.style.replace(/(?:^|\s)height\s*:\s*(\d+)px;?/i, '');
+                        el.attributes.height = height;
+                    }
+                }
+        
+                // Remove the style tag if it is empty
+                if (!el.attributes.style)
+                    delete el.attributes.style;
+            }
+        }
+    });
+});
 </script>
 </x-layouts.app>
