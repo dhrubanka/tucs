c79ad01218e379c45b1f1026d6aae922e4587c09
Dhrubanka Chutia <dhrubankachutia@gmail.com>
2022-08-24 15:01:22 +0530

    ui update

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -7,7 +7,7 @@
                         <div class="row">
                             <div class="col-12">
                                 <h4>{{$blog->title}}</h4>
-                                <small>By <a href="/profile/show/{{$blog->user->id}}">{{$blog->profile->firstName}}</a> </small> on {{$blog->created_at}}
+                                <small>By <a href="/profile/show/{{$blog->profile->user->id}}">{{$blog->profile->firstName}}</a> </small> on {{$blog->created_at}}
                                 <hr>
                                
                                 <p>{!!$blog->content!!}</p>
